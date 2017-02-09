0160368af0c8d3718798eb4d806774bf67a2b8b1
LeoVergara1 <leovergara.dark@gmail.com>
2017-02-09 13:13:14 -0600

    Pruebas

--- GroovyInter/Anotaciones/AST.groovy
+++ GroovyInter/Anotaciones/AST.groovy
@@ -29,3 +29,18 @@ class Email {
     )
   }
 }
+
+import groovy.json.JsonSlurper
+ 
+class BuscadorDeCervecerias {
+  static metodoApi = "search/beer"
+ 
+  static findByPais(String pais){
+    def cerveceras = []
+    def jsonSlurper = new JsonSlurper()
+    def httpConnection = new URL(ApiSystem.instance.getApiUrlRequest(metodoApi,[q:pais]))
+    def queryResult = jsonSlurper.parseText(httpConnection.text)
+    cerveceras = queryResult.response.breweries.items.collect { c -> Cerveceria.createWithParams(c.brewery) }
+    cerveceras
+  }
+}
